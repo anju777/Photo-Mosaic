751097724bcedf0009d8d274e45eef2147c709e8
anju777 <55717420+anju777@users.noreply.github.com>
2020-04-05 18:50:30 -0400

    Wrote most preliminary functions for MosaicPhotoOperator

--- imageMosaicOperator.py
+++ imageMosaicOperator.py
@@ -6,6 +6,7 @@
 
 from cmu_112_graphics import *
 from PIL import Image, ImageColor
+import os
 import timesheets
 
 ####################### Sample Images (Remove Later) ########################
@@ -13,35 +14,94 @@ fileName = "Nagahama_Neru"
 imgPath = f"C:\\Users\\anjua\\OneDrive\\Pictures\\15112_TP\\{fileName}.jpg"
 image1 = Image.open(imgPath)
 
-fileName = "hkt"
+fileName = "HKT"
 imgPath = f"C:\\Users\\anjua\\OneDrive\\Pictures\\15112_TP\\{fileName}.jpg"
 image2 = Image.open(imgPath)
 
-fileName = "planetEarth"
+fileName = "Nature"
 imgPath = f"C:\\Users\\anjua\\OneDrive\\Pictures\\15112_TP\\{fileName}.jpg"
 image3 = Image.open(imgPath)
+
+sampleImages = os.listdir("C:\\Users\\anjua\\OneDrive\\Pictures\\15112_TP\\")
+for i in range(len(sampleImages)):
+    text = sampleImages[i]
+    imgPath = f"C:\\Users\\anjua\\OneDrive\\Pictures\\15112_TP\\{text}"
+    sampleImages[i] = Image.open(imgPath)
 #############################################################################
 
+
+############################ MAIN FUNCTION #########################
 # Takes in an image object, and returns the image mosaic
-# Main function of the module
-def createMosaicFromImage(mainImage, listOfSampleImages):
-    ####### Replace later with rows/columns obtainer ##########
+def createMosaicFromImage(mainImage, sampleImages):
+    'Replace later with rows/columns obtainer '
     rows =  50
-    columns = 25
-    ###########################################################
-    sampleRGBValues = getListOfRGBValues(listOfSampleImages)
-    griddedImage = gridImage(mainImage, rows, columns)
+    cols = 25
 
-    for grid in range(len(griddedImage)):
-        avgRGB = getAverageRGB(griddedImage[grid])
-        indexOfSampleImage = findClosestValueFromList(avgRGB, sampleRGBValues)
-        sampleImage = listOfSampleImages[indexOfSampleImage]
-        griddedImage[grid] = sampleImage
+    sampleImages = sizeSampleImages(sampleImages, mainImage, rows, cols)
+    sampleImagesRGB = getListOfRGBValues(sampleImages)
+    griddedImages = gridImage(mainImage, rows, cols)
 
-    result = convertGridsToOriginal(griddedImage)
+    for row in range(rows):
+        for col in range(cols):
+            avgRGB = getAverageRGB(griddedImages[row][col])
+            indexOfSampleImage = findClosestValue(avgRGB, sampleImagesRGB)
+            sampleImage = sampleImages[indexOfSampleImage]
+            griddedImages[row][col] = sampleImage
+
+    result = convertGridsToOriginal(griddedImages)
     return result
 
-# Takes in an image object and obtains the average RGB in the image
+
+
+########################## sizeSampleImages ##############################
+# Takes in list of images and returns the same list of images, but sized to 
+# match the size of each grid if the mainImage was cut up in indicated rows/cols
+def sizeSampleImages(sampleImages, mainImage, rows, cols):
+    for i in range(len(sampleImages)):
+        sampleImages[i] = sizeImage(sampleImages[i], mainImage, rows, cols)
+    return sampleImages
+
+# Takes in an image and returns the image, sized to match the size of each grid 
+# if the mainImage was cut up in indicated rows/cols
+def sizeImage(image, mainImage, rows, cols):
+    '''Note: crops image (takes top left part) & resizes, so might need to 
+    modify later if want better selection/compression (just skip to resize
+    for latter), and fractal expansion'''
+    gridWidth, gridHeight = mainImage.width//cols, mainImage.height//rows
+    ratioHeight, ratioWidth = image.height/gridHeight, image.width/gridWidth
+    baseRatio = min(ratioHeight, ratioWidth)
+    targetWidth, targetHeight = gridWidth * baseRatio, gridHeight * baseRatio
+
+    image = image.resize((gridWidth, gridHeight), 3, 
+            (0, 0, targetWidth, targetHeight))
+    return image
+
+############################################################################
+
+# Takes in list of images and returns list (same len) of tuples containing RGB
+# values of each elements
+def getListOfRGBValues(imageList):
+    RGBList = []
+    for image in imageList:
+        RGBValue = getAverageRGB(image)
+        RGBList.append(RGBValue)
+    return RGBList
+
+# Takes in an image and returns a 2D list of the image with indicated rows/cols
+def gridImage(image, rows, cols):
+    result = []
+    gridWidth = image.width // cols
+    gridHeight = image.height // rows
+    for row in range(rows):
+        result.append([])
+        for col in range(cols):
+            grid = image.crop((col*gridWidth, row*gridHeight, 
+                    col*gridWidth + gridWidth, row*gridHeight + gridHeight))
+            result[row].append(grid)
+    return result
+
+########################## getAverageRGB #################################
+# Takes in an image object and returns a tuple of the average RGB of the image
 def getAverageRGB(image):
     # Modifies image so that it can be represented with 256 colors
     image = image.quantize()
@@ -59,7 +119,9 @@ def getAverageRGB(image):
     avgRGB = divideElements(avgRGB, pixelCount, 'int', tuple())
     return avgRGB 
 
-# Takes in colorCount & palette. Returns new list of (count, (RGB Value))
+# Takes in colorCount (list of tuples (count, paletteNumber)) & palette (list
+# of RGB values (not separated by tuples, etc.)). 
+# Returns new list in the format(count, (RGB Value))
 def combineListAndPalette(colorList, colorPalette):
     for i in range(len(colorList)):
         count = colorList[i][0]
@@ -71,9 +133,9 @@ def combineListAndPalette(colorList, colorPalette):
     return colorList
 
 # Takes in element and returns element with each element divided by divisor
-# Default return type is lists and integer divides, but can also specify
-# Mode: can integer divide ('int'), floor divide ('floor'), or 'float' divide
-# target: takes in object (list, tuple, or set) and returns result of that type
+#   Default return type is lists and integer divides, but can also specify
+#   Mode: can integer divide ('int'), floor divide ('floor'), or 'float' divide
+#   Target: Returns result in indicated object format (list, set, or tuples)
 def divideElements(L, divisor, mode='int', target=list()):
     mode = mode.lower()
     if (mode != 'int' and mode != 'floor' and mode != 'float'):
@@ -96,13 +158,32 @@ def divideElements(L, divisor, mode='int', target=list()):
     else:
         return 'Error: Target type must be object of type list, tuple, or set'
 
-
 def testGetAverageRGB():
     print('Testing getAverageRGB... ', end='')
     assert(getAverageRGB(image1) == (172, 169, 122))
     assert(getAverageRGB(image2) == (137, 109, 107))
     assert(getAverageRGB(image3) == (122, 117, 68))
     print('Passed!!')
+
+############################################################################
+
+# Takes in target value and a list, and returns the index closest to the target
+def findClosestValue(value, inputList):
+    smallestDifference = None
+    closestIndex = None
+    for i in range(len(inputList)):
+        difference = abs(value - inputList[i])
+        if (smallestDifference == None or difference < smallestDifference):
+            smallestDifference = difference
+            closestIndex = i
+    return closestIndex
+
+# Takes in 2D list containing image objects, and returns the images put together
+# Note: all sizes of images in the griddedImages must be the same
+def convertGridsToOriginal(griddedImages):
+    rows = len(gridedImages)
+    cols = len(griddedImages[0])
+    griddedImage = griddedImages[0][0]
 
 def main():
     testGetAverageRGB()
